976ea30c530c2b28fa3d2f86a256adc08cea9633
duleduba <111997014+duleduba@users.noreply.github.com>
2023-09-13 15:49:38 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -20,7 +20,7 @@ const BLOG = {
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '嗳眈媄', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '沉溺于美好的事物', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://aidanmei.com', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '小说,漫画,电视剧,泰剧,韩剧,韩漫,耽美,言情,资源站,废文,海棠，网盘', // 网站关键词 英文逗号隔开
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '小说,漫画,电视剧,泰剧,韩剧,韩漫,广播剧,耽美,言情,资源站,女频,废文,海棠,网盘', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || '', // 邮箱地址 例如mail@tangly1024.com
